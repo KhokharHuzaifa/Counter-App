9f1302d47225e6fd4f332b3944d56665538a71db
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-21 19:48:04 +0500

    Store updated

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { CounterSlice } from "./CounterReducer";
+// Persisting materaial imported
 import { persistStore , persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
